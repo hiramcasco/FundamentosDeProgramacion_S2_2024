b81931d4cee85a5f68e30251325930d2a093ec8a
hiramcasco <106841501+hiramcasco@users.noreply.github.com>
2024-02-22 16:53:29 -0300

    VPL primeras 2 semanas

---
+++ VPLsemana01y02.py
@@ -0,0 +1,29 @@
+#Hola Mundo
+#print("Hola Mundo")
+
+#Prueba VPL suma
+# primer_numero = int(input())
+# segundo_numero = int(input())
+# print("El primer digito de",primer_numero,"+",segundo_numero,"es:",(primer_numero+segundo_numero)%10)
+
+# Ejercicio 1:
+# Dada una cantidad principal 'P' una tasa de interés 'r' y un número de años 'n'
+# calcula la cantidad final 'A' usando la fórmula de interés compuesto: A=P(1+r100)n
+
+# _capital_0 = float(input())   #En $
+# _interes = float(input())   #Porcentaje
+# _tiempo = int(input())    #Años
+# _capital_f = _capital_0*(1 + _interes/100)**_tiempo
+
+# print("La cantidad final después de",_tiempo,"años es: $"+str(_capital_f))
+
+# Ejercicio 2: Velocidad final
+# Dada una velocidad inicial vi, una aceleración constante a y un tiempo t, calcula la velocidad
+# final vf usando la fórmula: vf=vi+at.
+
+# _velociadad_0 = float(input())
+# _aceleracion = float(input())
+# _tiempo = float(input())
+# _velocidad_f = _velociadad_0 + _aceleracion*_tiempo
+
+# print(f"La velocidad final después de {_tiempo:.1f} segundos es: {_velocidad_f:.1f} m/s")
